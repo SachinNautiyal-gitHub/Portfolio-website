57a00b5e78889d572b8d1f9a422c34b0a2e78a5a
sachinnautiyal-github <nautiyalsachin217@gmail.com>
2024-02-23 09:19:34 +0530

    changes

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,7 +4,7 @@ import styles from './skills.module.css'
 const Skills = () => {
   return (
     <div className={styles.skills} id='skills'>
-      <h1>Skills</h1>
+      <h1>Tech Skills</h1>
       
     </div>
   )
