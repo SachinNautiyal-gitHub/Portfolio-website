e2557d434af4416b1fe3e15459e4a495e00ea25b
sachinnautiyal-github <nautiyalsachin217@gmail.com>
2024-02-24 07:55:52 +0530

    changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,27 +7,7 @@ const About = () => {
 
       <div className={styles.about} id='about'>
 
-        <h1>About Me</h1>
-
-        <div className={styles.description}>
-          Welcome to my digital space! I am a passionate and versatile Full Stack Web Developer with a specialization in the MERN (MongoDB, Express.js, React, Node.js) stack and C++ development. My journey in the world of programming is marked by a deep understanding of Data Structures and Algorithms (DSA), which forms the backbone of my problem-solving skills.
-          <br />
-          In the dynamic realm of web development, I bring proficiency in crafting robust and scalable applications. From server-side logic in Node.js to creating responsive and intuitive user interfaces with React, I thrive on bringing ideas to life in the digital landscape. My expertise extends to working with MongoDB for efficient data management and Express.js for building robust and secure server applications.
-          <br />
-          <br />
-          Whether it's developing the backend logic, designing captivating front-end interfaces, or optimizing database performance, my goal is to create web applications that not only meet but exceed expectations. Join me on this exciting journey where technology meets innovation, and together, we can turn your ideas into reality. Explore my portfolio to witness the fusion of creativity, precision, and functionality in every project I undertake.
-          <br />
-
-          <div className={styles.buttons}>
-            <a href="https://www.upwork.com/freelancers/~01b7353984e10e506c?mp_source=share" target='_blank'><button className={styles.upworkButton} > Hire me on Upwork</button></a>
-            <a href="https://github.com/SachinNautiyal-gitHub" target='_blank'><button className={styles.githubButton}> GitHub</button></a>
-            <a href="https://www.linkedin.com/in/sachin-nautiyal-201682245/" target='_blank'><button className={styles.linkedinButton}>Linkedin</button></a>
-            <a href="/" target='_blank'><button className={styles.resumeButton}>My Resume</button></a>
-          </div>
-
-
-        </div>
-
+        
 
       </div>
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import styles from './home.module.css'
 import image from '../../images/my.png'
-import HtmlIcon from '../../images/html.png'
-import cssIcon from '../../images/css.png'
-import tailwindIcon from '../../images/tailwind.png'
-import bootstrapIcon from '../../images/bootstrap (2).png'
-import javaScriptIcon from '../../images/javascript.png'
-import mongoIcon from '../../images/mongodb (2).png'
-import expressIcon from '../../images/expressjs.png'
-import ReactIcon from '../../images/react.png'
-import nodeJsIcon from '../../images/nodejs.png'
-import NextIcon from '../../images/nextjs.png'
-import cPIcon from '../../images/c++.png'
-import NetCoreIcon from '../../images/netcore.png'
-import pyhtonIcon from '../../images/python.png'
-import AngularIcon from '../../images/angular.png'
+// import HtmlIcon from '../../images/html.png'
+// import cssIcon from '../../images/css.png'
+// import tailwindIcon from '../../images/tailwind.png'
+// import bootstrapIcon from '../../images/bootstrap (2).png'
+// import javaScriptIcon from '../../images/javascript.png'
+// import mongoIcon from '../../images/mongodb (2).png'
+// import expressIcon from '../../images/expressjs.png'
+// import ReactIcon from '../../images/react.png'
+// import nodeJsIcon from '../../images/nodejs.png'
+// import NextIcon from '../../images/nextjs.png'
+// import cPIcon from '../../images/c++.png'
+// import NetCoreIcon from '../../images/netcore.png'
+// import pyhtonIcon from '../../images/python.png'
+// import AngularIcon from '../../images/angular.png'
 
 
 import githubIcon from '../../images/github.png'
 import linkedinIcon from '../../images/linkedin.png'
+import upworkIcon from '../../images/upwork2.png'
 
 
 
@@ -29,7 +30,7 @@ const Home = () => {
             <div className={styles.home} id='home'>
                 <div className={styles.con}>
 
-                <div className={styles.image}>
+                    <div className={styles.image}>
                         <img src={image} alt="" />
                     </div>
                     <div className={styles.contant}>
@@ -38,23 +39,28 @@ const Home = () => {
                         <p className={styles.text_P2}>Full-Stack Web Developer</p>
 
                         <div className={styles.buttons}>
-                           <a href=""><button className={styles.button1}>Download CV</button></a>
-                           <a href="#contact" style={{scrollBehavior:'smooth',}}><button className={styles.button2}>Contact Me</button></a>
+                            <a href="/"><button className={styles.button1}>Download CV</button></a>
+                            <a href="#contact" style={{ scrollBehavior: 'smooth', }}><button className={styles.button2}>Contact Me</button></a>
                         </div>
 
                         <div className={styles.socialMedia}>
-                          <a href=""><img src={linkedinIcon} alt="" /></a>
-                          <a href="/"><img src={githubIcon} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/sachin-nautiyal-201682245/" rel="noreferrer" target='_blank'><img src={linkedinIcon} alt="" /></a>
+                            <a href="https://github.com/SachinNautiyal-gitHub" rel="noreferrer" target='_blank'><img src={githubIcon} alt="" /></a>
+                        </div>
+
+                        <div className={styles.upwork}>
+
+                            <p>Hire Me On </p><a href="https://www.upwork.com/freelancers/~01b7353984e10e506c" rel='noreferrer'  target='_blank'><img className={styles.upworkImage} src={upworkIcon} alt="upwork logo" /></a>
                         </div>
                     </div>
 
 
 
-                    
-                </div>
-               
 
-               <div className={styles.techSTack}>
+                </div>
+
+
+                {/* <div className={styles.techSTack}>
                 <h2>Tech STack</h2>
                  <img src={HtmlIcon} alt="" />
                  <img src={cssIcon} alt="" />
@@ -73,7 +79,7 @@ const Home = () => {
 
 
 
-               </div>
+               </div> */}
 
             </div>
 
